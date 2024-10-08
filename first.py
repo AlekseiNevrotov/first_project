fc4c4e7825036374eb3b145981af061c9e6e482b
Алексей Невротов <elazarsound@gmail.com>
2024-10-08 17:21:13 +0300

    Update and rename practicum.py to first.py

---
+++ first.py
@@ -0,0 +1,8 @@
+'''sdsdsd'''
+def say_hello():
+    print('Привет!')     
+pass
+pass
+def saa();
+разработчик elazar
+по всем вопросам
--- practicum.py
+++
@@ -1,8 +0,0 @@
-'''sdsdsd'''
-def say_hello():
-    print('Привет, Практикуdм!')     
-pass
-pass
-def saa();
-разработчик elazar
-по всем вопросам
